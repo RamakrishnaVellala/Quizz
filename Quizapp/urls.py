eb7f5181e86835da50ffc3c964171bd02d9a4710
RamakrishnaVellala <itsp.kadapa@yahoo.com>
2022-07-29 16:59:47 +0530

    added

--- Quizapp/urls.py
+++ Quizapp/urls.py
@@ -8,9 +8,9 @@ router.register(r'quiz', views.QuizViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('home/', views.home, name="home"),
+    path('', views.home, name="home"),
     path('register/', views.register, name='register'),
     path('login/', views.Login, name='login'),
     path('logout/', views.Logout, name='logout'),
